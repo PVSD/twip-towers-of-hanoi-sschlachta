9277753d2c24884f2950b2d05792569cc0b764b8
sschlachta <sydneyschlachta@gmail.com>
2019-01-14 11:16:19 -0500

    yeet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Tower of Hanoi Game. \n " +
+                "This program will solve this problem for you.\n " +
+                "Please input number of disks to solve for: ");
+
+        Scanner sbr = new Scanner(System.in);
+        Solve han = new Solve(sbr.nextInt());
+        
+
     }
 }
---
+++ src/com/company/Solve.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by ss772 on 1/14/19.
+ */
+public class Solve {
+
+    public int discs;
+
+    public Solve(int d){
+
+        discs = d;
+
+    }
+
+    public void solve(){
+
+
+
+    }
+
+}
